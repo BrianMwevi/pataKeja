cf3a76db245c094ae68fb4679044edecbac70890
Willbeckh <wileyhndirangu04@gmail.com>
2022-03-22 14:38:23 +0300

    ft: featured-toggle-buttons-- add event listeners for toggle effect

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,13 @@
 $(document).ready(function () {
-	$("body").css("background-color", "cyan")
+  // $("body").css("background-color", "cyan")
+  slideFeatures();
 });
+
+// featured section slider
+let slideFeatures = () => {
+  $(".prev").click(function () {
+    console.log("prev btn pressed");
+  });$(".next").click(function () {
+    console.log("next btn pressed");
+  });
+};
