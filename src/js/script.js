676ae4fd6920ffa5f2a906fa145f467a4df65bd0
Willbeckh <wileyhndirangu04@gmail.com>
2022-03-24 01:15:40 +0300

    bg: remove carousel slide code

--- src/js/script.js
+++ src/js/script.js
@@ -1,32 +1,3 @@
 $(document).ready(function () {
   console.log("page loaded");
-  slider();
 });
-
-// featured section slider
-let slider = () => {
-  $(".next").click(function () {
-    let currentSlide = $(".slide.active");
-    let nextSlide = currentSlide.next();
-
-    currentSlide.fadeOut(400).removeClass("active");
-    nextSlide.fadeIn(400).addClass("active");
-
-    if (nextSlide.length == 0) {
-      $(".slide").first().fadeIn(300).addClass("active");
-    }
-  });
-
-  $(".prev").click(function () {
-    console.log("prev btn pressed");
-    let currentSlide = $(".slide.active");
-    let prevSlide = currentSlide.prev();
-
-    currentSlide.fadeOut(300).removeClass("active");
-    prevSlide.fadeIn(300).addClass("active");
-
-    if (prevSlide.length == 0) {
-      $(".slide").last().fadeIn(300).addClass("active");
-    }
-  });
-};
